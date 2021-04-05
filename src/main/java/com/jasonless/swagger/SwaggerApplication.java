5c560fcf41f769e25bd3941124438b62373e92af
Starrk <liushizeng45@163.com>
2021-04-05 14:38:27 +0800

    init

---
+++ src/main/java/com/jasonless/swagger/SwaggerApplication.java
@@ -0,0 +1,15 @@
+package com.jasonless.swagger;
+
+import com.jasonless.swagger.build.TemplateBuilder;
+
+/**
+ * @author Jasonless
+ * @date 2021/4/3
+ */
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        TemplateBuilder.builder();
+    }
+
+}
